8c158a1927ec9d1e1c56096d64d4d69432e2ca46
indraprasta123 <indraprastadwinanda@gmail.com>
2026-02-26 10:17:05 +0700

    page doctors,diseases

--- controllers/doctorsController.js
+++ controllers/doctorsController.js
@@ -17,6 +17,135 @@ class DoctorsController {
       res.send(error);
     }
   }
+
+  static async getAddDoctors(req, res) {
+    try {
+      let doctors = await Doctor.findAll();
+      res.render("addDoctors", { doctors });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async postAddDoctors(req, res) {
+    try {
+      const { name, specialization, degree, imageUrl } = req.body;
+      await Doctor.create({ name, specialization, degree, imageUrl });
+      res.redirect("/doctors");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async getDeleteDoctors(req, res) {
+    try {
+      const { id } = req.params;
+      await Doctor.destroy({
+        where: {
+          id,
+        },
+      });
+      res.redirect("/doctors");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async getEditDoctors(req, res) {
+    try {
+      const { id } = req.params;
+      let doctors = await Doctor.findByPk(id);
+      res.render("editDoctors", { doctors });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async postEditDoctors(req, res) {
+    try {
+      const { id } = req.params;
+      const { imageUrl, name, specialization, degree } = req.body;
+      await Doctor.update(
+        { imageUrl, name, specialization, degree },
+        { where: { id } },
+      );
+      res.redirect("/doctors");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async diseases(req, res) {
+    try {
+      let diseases = await Disease.findAll();
+      res.render("diseases", { diseases });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async getAddDiseases(req, res) {
+    try {
+      let diseases = await Disease.findAll();
+      res.render("addDiseases", { diseases });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async postAddDiseases(req, res) {
+    try {
+      const { name, description, specialization, level, medicine } = req.body;
+      await Disease.create({
+        name,
+        description,
+        specialization,
+        level,
+        medicine,
+      });
+      res.redirect("/doctors/diseases");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async getDeleteDiseases(req, res) {
+    try {
+      const { id } = req.params;
+      await Disease.destroy({
+        where: {
+          id,
+        },
+      });
+      res.redirect("/doctors/diseases");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async getEditDiseases(req, res) {
+    try {
+      const { id } = req.params;
+      let diseases = await Disease.findByPk(id);
+      res.render("editDiseases", { diseases });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async postEditDiseases(req, res) {
+    try {
+      const { id } = req.params;
+      const { name, description, specialization, level, medicine } = req.body;
+      await Disease.update(
+        { name, description, specialization, level, medicine },
+        { where: { id } },
+      );
+      res.redirect("/doctors/diseases");
+    } catch (error) {
+      res.send(error);
+    }
+  }
 }
 
 module.exports = DoctorsController;
--- routers/doctors.js
+++ routers/doctors.js
@@ -2,5 +2,28 @@ const router = require("express").Router();
 const DoctorsController = require("../controllers/doctorsController");
 
 router.get("/", DoctorsController.doctors);
+router.get("/addDoctors", DoctorsController.getAddDoctors);
+router.post("/addDoctors", DoctorsController.postAddDoctors);
+
+// delete doctors
+router.get("/deleteDoctors/:id", DoctorsController.getDeleteDoctors);
+
+//edit doctors
+router.get("/:id/editDoctors", DoctorsController.getEditDoctors);
+router.post("/:id/editDoctors", DoctorsController.postEditDoctors);
+
+//read diseases
+router.get("/diseases", DoctorsController.diseases);
+
+//add diseases
+router.get("/addDiseases", DoctorsController.getAddDiseases);
+router.post("/addDiseases", DoctorsController.postAddDiseases);
+
+//delete diseases
+router.get("/deleteDiseases/:id", DoctorsController.getDeleteDiseases);
+
+//edit doctors
+router.get("/:id/editDiseases", DoctorsController.getEditDiseases);
+router.post("/:id/editDiseases", DoctorsController.postEditDiseases);
 
 module.exports = router;
